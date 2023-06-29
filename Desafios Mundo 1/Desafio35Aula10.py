98dc2db7ee29e1cf74eeea8ce26c9320096991df
Gabriel Scaranello <138091822+Gabscara@users.noreply.github.com>
2023-06-29 16:12:02 -0300

    Update Desafio35Aula10.py

--- Desafios Mundo 1/Desafio35Aula10.py
+++ Desafios Mundo 1/Desafio35Aula10.py
@@ -1,24 +1,22 @@
 from time import sleep
-r1 = int(input('Primeira reta:'))
-r2 = int(input('Segunda reta:'))
-r3 = int(input('Terceira reta:'))
-rt = r1+r2
-if rt >= r3:
+r1 = int(input('Primeira segmento:'))
+r2 = int(input('Segunda segmento:'))
+r3 = int(input('Terceira segmento:'))
+if r1 < r2+r3 and r2 < r1+r3 and r3 < r1+r2:
     print(f'O comprimento da 1º-"{r1}"')
-    print(' '*16, f'2º-"{r2}"')
-    print(' '*16, f'3º-"{r3}')
-    print(' '*16, f'Loading...')
+    print(' ' * 16, f'2º-"{r2}"')
+    print(' ' * 16, f'3º-"{r3}')
+    print(' ' * 16, f'\033[1;35mLoading...\033[m')
     sleep(3)
-    print(f'É um triângulo porque a soma do 1ºNúmero:"{r1}" E')
-    print(' '*31, f'2ºNúmero:"{r2}" é maior que a 3º Reta = {r3} ')
+    print('Pode formar um \033[1;32mTRIÂNGULO\033[m')
     sleep(2)
-    print(f'Gemotria DASH!!')
-
-elif rt < r3:
+    print('\033[1;35mGemotria DASH!!\033[m')
+    print('\033[1;31mRegra:\033[m A soma de dois segmentos menores tem de ser maior que o 3º segmento')
+else:
     print(f'O comprimento da 1º-"{r1}"')
     print(' ' * 16, f'2º-"{r2}"')
     print(' ' * 16, f'3º-"{r3}"')
-    print(' ' * 16, f'Loading...')
+    print(' ' * 16, f'\033[1;35mLoading...\033[m')
     sleep(3)
-    print(f'Não é um triângulo porque a soma do 1ºNúmero:"{r1}" E')
-    print(' ' * 35, f'2ºNúmero:"{r2}" não é maior que a 3º Reta = {r3} ')
+    print(f'Os segmentos mencionados acima não pode formar um \033[1;32mTRIÂNGULO\033[m')
+    print('\033[1;31mRegra:\033[m A soma de dois segmentos menores tem de ser maior que o 3º segmento')
