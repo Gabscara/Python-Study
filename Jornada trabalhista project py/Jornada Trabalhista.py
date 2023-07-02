d241543caa421eb3018c8c4a6784a93eb6f5cda7
Gabriel Scaranello <138091822+Gabscara@users.noreply.github.com>
2023-07-02 15:38:05 -0300

    Update Jornada Trabalhista.py

--- Jornada trabalhista project py/Jornada Trabalhista.py
+++ Jornada trabalhista project py/Jornada Trabalhista.py
@@ -5,9 +5,10 @@ semanasano = 365/7
 hrd = str(input('Quantas horas você dorme por dia?\n\033[1;31mExemplo: "8:30"\033[m\n\033[1;30mINFORME:\033[m')).strip()
 hrd = hrd.replace(':', '.')
 horasdormindo = float(hrd)
-l = str(input('Quantas horas passa se locomovendo?\n\033[1;31mExemplo: "2:30"\033[m\n\033[1;30mINFORME:\033[m')).strip()
-l = l.replace(':', '.')
-locomocao = float(l)
+loc = str(input('Quantas horas passa se locomovendo?\n\033[1;31mExemplo: "2:30"\033[m\n'
+                '\033[1;30mINFORME:\033[m')).strip()
+loc = loc.replace(':', '.')
+locomocao = float(loc)
 r = str(input('Quantas horas você tem de almoço?\n\033[1;31mExemplo: "1:30"\033[m\n\033[1;30mINFORME:\033[m')).strip()
 r = r.replace(':', '.')
 refeicoes = float(r)
@@ -29,27 +30,30 @@ gastm = float(gast)
 
 #Variaveis de tempo livre na jornada
 horasrest = 24.00 - (trabalho+refeicoes+locomocao+horasdormindo)
-sobra_hora_ano = horasrest * (diasdasemana*semanasano)
-diasfrest = 365 - (diasdasemana*semanasano)
-httf = diasfrest*24
-horaslivre = httf+sobra_hora_ano
+sobra_hora_ano = horasrest * (diasdasemana*52)
+httf = 104*24
+horaslivre = httf + sobra_hora_ano
 temposobra = horaslivre * anosmorte
 resultado1 = temposobra/24
 resultadof = resultado1/365
 horaslivrem = horaslivre/12
-horaslivred = horaslivrem/24
-
+horaslivred = horaslivrem*0.0416667
 #Variaveis de trabalho
 horastrab = trabalho+locomocao
-trab_hora_ano = horastrab * (diasdasemana*semanasano)
+trab_hora_ano = horastrab * (diasdasemana*52)
 tempotrab = trab_hora_ano * anosmorte
 resultado2 = tempotrab/24
 resultadof2 = resultado2/365
-horasmes = horastrab*(diasdasemana*4.34524)
-
+horasmes = horastrab*(diasdasemana*4)
+#médias totais
+media_sobra_rest = (horaslivre/8736) * 100
+media_trab = (trab_hora_ano/8736) * 100
+media_ali_dorm = 8736 - (horaslivre+trab_hora_ano)
+media_rest = (media_ali_dorm/8736) * 100
+media_livre = (horaslivre-media_ali_dorm)/8736
 #Variaveis salarios
 faixasalarial = faixa/horasmes
-horaspdia = horasmes/(diasdasemana*4.34524)
+horaspdia = horasmes/(diasdasemana*4)
 faixarest = faixa-gastm
 faixaresta = (faixarest/horasmes)*trab_hora_ano
 
@@ -58,27 +62,30 @@ print(f'\033[7;34;40m{"-=-"*20}\033[m\n'
       f'\033[1;33mSobre o tempo restante livre, você tera um total de:\033[m\n'
       f'\033[7;34;40m{"-=-"*20}\033[m\n'
       f'\033[1;33mTEMPO,TIPO\033[m\n'
-      f'\033[1;35m{int(resultadof.__ceil__()):,.0f} '
+      f'\033[1;35m{resultadof.__ceil__():,.0f} '
       f'ANOS LIVRES\033[m em \033[1;35m{anosmorte:.0f} Anos Vividos\033[m\n'
-      f'\033[1;35m{int(resultado1.__ceil__()):,.0f} '
+      f'\033[1;35m{resultado1.__ceil__():,.0f} '
       f'DIAS LIVRES\033[m em \033[1;35m{anosmorte*365:,.0f} Dias Vividos\033[m\n'
-      f'\033[1;35m{int(temposobra.__ceil__()):,.0f} '
+      f'\033[1;35m{temposobra.__ceil__():,.0f} '
       f'HRS TOTAIS LIVRES\033[m em \033[1;35m{(anosmorte*365)*24:,.0f} Horas Vividas\033[m\n'
-      f'\033[1;35m{int(horaslivre/24):,.0f}\033[m Dias livres em um ANO\n'
-      f'\033[1;35m{int(horaslivrem/24):,.0f}\033[m Dias livres em um MÊS\n'
-      f'\033[1;35m{(horaslivred/24):,.2f}\033[m Média de DIA livre contando finais de semana\n'
-      f'\033[1;35m{int(horaslivre.__ceil__()):,.0f}\033[m '
-      f'HORAS LIVRES\033[m em um ANO\n'
-      f'\033[1;35m{int(horaslivrem.__ceil__()):,.0f} '
-      f'HORAS LIVRES\033[m em um MÊS\n'
-      f'\033[1;35m{int(horaslivred.__ceil__()):,.0f} '
-      f'HORAS LIVRES\033[m em um DIA')
+      f'\033[7;34;40m{"-=-"*20}\033[m\n'
+      f'Nos \033[1;35m{diasdasemana:.0f} Dias da semana\033[m identifiquei \033[1;35m{horasrest} Hrs'
+      f'\033[m livres em cada dia.\n as "horas livres restantes" + "horas livres finais de semana" '
+      f'apresenta uma média de:\n'
+      f'\033[7;34;40m{"-=-"*20}\033[m\n'
+      f'\033[1;33mConvertendo as horas totais livres em um ANO...\033[m\n'
+      f'\033[1;35mSão exatamente: "{horaslivre:,.0f}"\033[m HRS , \033[1;35m"{horaslivre/24:.0f}"\033[m DIAS e '
+      f'\033[1;35m"{horaslivre*0.00136986:.0f}"\033[m MESES Livres '
+      f'Média de \033[1;35m"{(horaslivrem/730)*100:,.1f}%"\033[m Dias livres a cada MÊS\n'
+      f'Média de \033[1;35m{horaslivrem.__ceil__()/24:.0f} Dias\033[m Livres em cada MÊS durante um ANO\n'
+      f'Média de \033[1;35m{horaslivrem:,.0f} HORAS\033[m LIVRES p/MÊS em um ANO\n'
+      f'Média de \033[1;35m{horaslivrem/30:,.0f} HORAS\033[m LIVRES p/DIA em um ANO\n')
 
 print(f'\033[7;34;40m{"-=-"*20}\033[m\n'
       f'\033[1;33mSobre o tempo trabalho:\033[m\n'
       f'\033[7;34;40m{"-=-"*20}\033[m\n'
       f'\033[1;35m{trab_hora_ano:,.0f} HRS em um ano!!!\033[m\n'
-      f'\033[1;35m{trab_hora_ano/12:.0f} HRS em um mês...\033[m\n'
+      f'\033[1;35m{horasmes:.0f} HRS em um mês...\033[m\n'
       f'\033[1;35m{horaspdia:.0f} HRS por dia\033[m\n'
       f'\033[7;34;40m{"-=-"*20}\033[m\n'
       f'\033[1;33mSobre seu salário:\033[m\n'
@@ -90,3 +97,14 @@ print(f'\033[7;34;40m{"-=-"*20}\033[m\n'
       f' mensal...\033[m\n'
       f'Se guardar 10% desse valor no decorrer de um ano receberá:\n'
       f'\033[1;35m{faixaresta*0.10:,.2f}R$\033[m')
+
+print(f'\033[7;34;40m{"-=-"*20}\033[m\n'
+      f'\033[1;31mA Conclusão é que se um ano contém:\033[m \033[1;35m8,736 Hrs\033[m\nDessas horas:\n'
+      f'\033[7;34;40m{"-=-"*20}\033[m\n'
+      f'Você terá \033[1;35m{horaslivre:,.0f} HRS ou {media_sobra_rest:.1f}% de HORAS\033[m LIVRES por ANO\n'
+      f'Você terá \033[1;35m{trab_hora_ano:,.0f} HRS ou {media_trab:.1f}% de HORAS\033[m TRABALHADAS por ANO'
+      f'\n'
+      f'Você terá \033[1;35m{media_ali_dorm:,.0f} HRS ou {media_rest:.1f}% de HORAS\033[m '
+      f'DORMINDO e se ALIMENTANDO por ANO\n'
+      f'Considerando que você dormir e se alimentar não entra em tempo livre:\n'
+      f'Você terá \033[1;35m{horaslivre-media_ali_dorm:,.0f} HRS ou {(media_livre) * 100:.1f}% de HORAS por ANO')
